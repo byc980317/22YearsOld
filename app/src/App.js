466d957f17089d8a2807ce8a8c0d19a22e37721e
xshi1012 <xshi@ucsd.edu>
2020-10-15 13:11:36 -0700

    frontend setup

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+import HomePage from './Views/Homepage';
+import './App.css';
+
+function App() {
+  return (
+      <HomePage/>
+  );
+}
+
+export default App;
