e0462a1af50bb3bb79d159f69d278c658ccb33da
byc980317 <yib002@ucsd.edu>
2020-10-17 18:07:58 -0400

    sign up using amplify

--- app/src/Views/SignUp.js
+++ app/src/Views/SignUp.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
-
+import { Auth } from "aws-amplify"
 import stringConstants from '../Constants/Strings';
 
 const SignUpForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    function passwordMatch() {
+        let match = password === confirmPassword;
+        return match;
+    }
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        
+        try {
+          await Auth.signUp({
+            username:email,
+            password,
+          })
+          alert("Sign up successfully");
+        } catch (error) {
+          console.log("Error in sign up", error);
+        }
+    }
+
     return (
     <Grid centered style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 350 }}>
         <Header as='h2' color='black' textAlign='center'>
             {stringConstants.SIGNUP_PROMPT}
         </Header>
-        <Form size='large'>
+        <Form size='large' onSubmit={handleSubmit}>
             <Segment stacked>
             <Form.Input
                 label='Email'
                 fluid icon='user'
                 iconPosition='left'
                 placeholder='E-mail Address'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
             />
             <Form.Input
                 label='Password'
@@ -25,6 +54,8 @@ const SignUpForm = () => {
                 iconPosition='left'
                 placeholder='Password'
                 type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
             />
             <Form.Input
                 label='Confirm password'
@@ -33,9 +64,11 @@ const SignUpForm = () => {
                 iconPosition='left'
                 placeholder='Confirm Password'
                 type='password'
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
             />
 
-            <Button color='green' fluid size='large'>
+            <Button disabled={!validateForm() || !passwordMatch()}color='green' fluid size='large' type='submit'>
                 Sign Up
             </Button>
             </Segment>
